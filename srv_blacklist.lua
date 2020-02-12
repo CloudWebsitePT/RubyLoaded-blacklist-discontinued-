7eee7df3320ab68cb026cc06c4321f171344f8cd
Rubylium <alexisoko0@gmail.com>
2020-02-12 20:59:09 +0100

    Blacklist is now in JSON

--- srv_blacklist.lua
+++ srv_blacklist.lua
@@ -1,39 +1,16 @@
 
 -- Wbehook ou toute les informations des personnes qui se connecte / essaye de se connecté son envoyé, à changer !
-webhook = "Votre webhook ici :D"
+webhook = "Votre webhook ici"
 
 -- Message de ban, c'est préférable de laisser celui la :)
-local blacklist = "RUBY-AC RELOADED - Vous avez été définitivement blacklist de tout les serveur sous protection Ruby-AC RELOADED dû à vos précédentes(s) action (Cheat, troll, dump etc...).\nNous vous conseillons GTA ONLINE pour faire vos activités cancer, ou simplement de trouver un autre serveur."
-
--- Nom de votre serveur, à changer !
-local NomDeVotreServeur = "RedSide"
-
--- Permet d'empecher les joueurs ayant des couleur dans leur pseudo de se connecter
-local AntiCouleur = true
-
-local AntiCouleurMessage = "RUBY-AC RELOADED - Vous n'êtes pas autorisé à utiliser des couleurs dans votre pseudo, merci de les retirer avant de rejoindre le serveur."
+local blacklistCheat = "RubyLoaded - Vous avez été définitivement blacklist de tout les serveur sous protection RubyLoaded dû à vos précédentes(s) action.\n\nInformation externe\nRaison: Utilisation de logiciel externe (Lua Injecteur, Mod Menu)\nVotre blacklist est définitive, le staff RubyLoaded **ne peut pas** vous aider.\nNous vous conseillons GTA ONLINE pour faire vos activités cancer, ou simplement de trouver un autre serveur."
 
 
-local colors = {
-	"~r~",
-	"~b~", 
-	"~g~",
-	"~y~",
-	"~p~", 
-	"~o~", 
-	"~c~", 
-	"~m~", 
-	"~u~",
-	"^1",
-	"^2",
-	"^3",
-	"^4",
-	"^5",
-	"^6",
-	"^7",
-	"^8",
-	"^9",
-}
+local blacklistAchatModMenu = "RubyLoaded - Vous avez été blacklist de tout les serveur sous protection RubyLoaded dû à vos précédentes(s) action.\n\nInformation externe\nRaison: Achats de Mod Menu / Cheat.\nVotre blacklist n'est pas définitive, le staff RubyLoaded **peut ** vous aider ici: https://discord.gg/YZFXv4Y."
+
+local blacklistVenteCheat = "RubyLoaded - Vous avez été définitivement blacklist de tout les serveur sous protection RubyLoaded dû à vos précédentes(s) action.\n\nInformation externe\nRaison: Vente de logiciel de triche (Injecteur LUA, Mod Menu etc...)\nVotre blacklist est définitive, le staff RubyLoaded **ne peut pas** vous aider.\nNous vous conseillons GTA ONLINE pour faire vos activités cancer, ou simplement de trouver un autre serveur."
+
+
 
 AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
 	identifiers = GetPlayerIdentifiers(source)
@@ -44,60 +21,81 @@ AddEventHandler('playerConnecting', function(playerName, setKickReason, deferral
 	deferrals.update("Ruby RELOADED - Vérification de la blacklist, merci de patienter ...\nTriages des cancers en cours ....")
 	Wait(100)
 	local blacklisted = false
-	PerformHttpRequest("https://raw.githubusercontent.com/Rubylium/RubyLoaded-blacklist/master/blacklist.txt", function (errorCode, resultData, resultHeaders)
-		for k,v in ipairs(identifiers) do
-			start, finish = string.find(resultData, v)
-			if start ~= nil and finish ~= nil then
-				CheckNewId(identifiers, resultData, playerName)
-				local message = "**Joueur blacklist** - Le joueur **"..playerName.."** à essayer de rejoindre le serveur **"..NomDeVotreServeur.."** alors qu'il à été blacklist par le système anti cancer.\nIdentifiant banni: "..v
-				CancelEvent()
-				print("^3RubyLoaded - ^1JOUEUR BLACK LIST POUR L'ID: "..v.."^7")
-				DropPlayer(source, blacklist)
-				deferrals.done(blacklist)
-				blacklisted = true
-				return
-			else
-				print('Player: ' .. playerName .. ', Identifier #' .. k .. ': ' .. v.."^2 autorisé^7")
+	PerformHttpRequest("https://raw.githubusercontent.com/Rubylium/RubyLoaded-blacklist/master/blacklist.json", function (errorCode, resultData, resultHeaders)
+		local blacklist = json.decode(resultData)
+		for _,v in ipairs(identifiers) do
+			for _, i in pairs(blacklist.cheat) do
+				if i == v then
+					CheckNewId(identifiers, blacklist, playerName)
+					print("^3RubyLoaded - ^1JOUEUR BLACK LIST POUR L'ID: "..v.."^7")
+					deferrals.done(blacklistCheat)
+					blacklisted = true
+					return
+				end
 			end
-		end
 
+			for _, i in pairs(blacklist.AchatMenu) do
+				if i == v then
+					CheckNewId(identifiers, blacklist, playerName)
+					print("^3RubyLoaded - ^1JOUEUR BLACK LIST POUR L'ID: "..v.."^7")
+					deferrals.done(blacklistAchatModMenu)
+					blacklisted = true
+					return
+				end
+			end
 
-		if AntiCouleur then
-			for _, color in pairs(colors) do
-				start, finish = string.find(playerName, color)
-				if start ~= nil and finish ~= nil then
-					local message = "**Couleur Interdite** - Le joueur "..playerName.." à essayer de rejoindre le serveur **"..NomDeVotreServeur.."** avec des couleurs dans son pseudo. ("..color..")"
-					deferrals.done(AntiCouleurMessage)
-					CancelEvent()
+			for _, i in pairs(blacklist.VenteCheat) do
+				if i == v then
+					CheckNewId(identifiers, blacklist, playerName)
+					print("^3RubyLoaded - ^1JOUEUR BLACK LIST POUR L'ID: "..v.."^7")
+					deferrals.done(blacklistVenteCheat)
+					blacklisted = true
 					return
 				end
 			end
 		end
+
 		if not blacklisted then
 			deferrals.update("Ruby RELOADED - Connexion autorisée ! Bon jeux !")
 			Wait(1000)
 			print("^3RubyLoaded - ^2Connexion autorisé pour "..playerName.."^7")
-			local message = "**RubyLoaded** - Connexion autorisé pour "..playerName.." sur **"..NomDeVotreServeur.."**"
 			deferrals.done()
 		end
 	end)
 end)
 
-
 RegisterNetEvent("RubyLoaded:VerifId")
 AddEventHandler("RubyLoaded:VerifId", function()
-	identifiers = GetPlayerIdentifiers(source)
-	_source = source
-	PerformHttpRequest("https://raw.githubusercontent.com/Rubylium/RubyLoaded-blacklist/master/blacklist.txt", function (errorCode, resultData, resultHeaders)
-		for k,v in ipairs(identifiers) do
-			local start, finish = string.find(resultData, v)
-			if start ~= nil and finish ~= nil then
-				CheckNewId(identifiers, resultData, GetPlayerName(_source))
-				print("^3RubyLoaded - ^1JOUEUR BLACK LIST POUR L'ID: "..v.."^7")
-				DropPlayer(_source, blacklist)
-				return
+	local identifiers = GetPlayerIdentifiers(source)
+	local _source = source
+	PerformHttpRequest("https://raw.githubusercontent.com/Rubylium/RubyLoaded-blacklist/master/blacklist.json", function (errorCode, resultData, resultHeaders)
+		local blacklist = json.decode(resultData)
+		for _,v in ipairs(identifiers) do
+			for _, i in pairs(blacklist.cheat) do
+				if i == v then
+					CheckNewId(identifiers, blacklist, playerName)
+					DropPlayer(_source, GetPlayerName(_source))
+					return
+				end
+			end
+
+			for _, i in pairs(blacklist.AchatMenu) do
+				if i == v then
+					CheckNewId(identifiers, blacklist, playerName)
+					DropPlayer(_source, GetPlayerName(_source))
+					return
+				end
+			end
+
+			for _, i in pairs(blacklist.VenteCheat) do
+				if i == v then
+					CheckNewId(identifiers, blacklist, playerName)
+					DropPlayer(_source, GetPlayerName(_source))
+					return
+				end
 			end
 		end
+
 	end)
 end)
 
@@ -126,17 +124,38 @@ end)
 function CheckNewId(identifiers, resultData, playerName)
 	local NewId = ""
 	local DejaBan = ""
+	local type = ""
 	for k,v in ipairs(identifiers) do
-		start, finish = string.find(resultData, v)
-		if start == nil and finish == nil then
-			NewId = NewId..v.."\n"
-		else
-			DejaBan = DejaBan..v.."\n"
+		for _, i in pairs(resultData.cheat) do
+			if i == v then
+				DejaBan = DejaBan..v.."\n"
+			else
+				NewId = NewId..v.."\n"
+				type = type.."Cheat "
+			end
+		end
+
+		for _, i in pairs(resultData.AchatMenu) do
+			if i == v then
+				DejaBan = DejaBan..v.."\n"
+			else
+				NewId = NewId..v.."\n"
+				type = type.."Achat de menu "
+			end
+		end
+
+		for _, i in pairs(resultData.VenteCheat) do
+			if i == v then
+				DejaBan = DejaBan..v.."\n"
+			else
+				NewId = NewId..v.."\n"
+				type = type.."Vente Cheat "
+			end
 		end
 	end
 
 	if #NewId > 0 then
-		local message = "**Pseudo:**"..playerName.."\n**ID Déja banni:**\n"..DejaBan.."\n**Nouveaux ID:**\n"..NewId
+		local message = "**Pseudo:**"..playerName.."\n**Type de ban**: "..type.."\n**ID Déja banni:**\n"..DejaBan.."\n**Nouveaux ID:**\n"..NewId
 
 		local discordInfo = {
 			["color"] = "15158332",
